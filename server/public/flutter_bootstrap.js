ff69c2bd0070e311777bb578b21b29154be698a3
José Oscar Ernesto Romero Campos <oscar.romero@anda.gob.sv>
2026-09-04 10:17:50 -0600

    feat: complete human profile editing, Cloudflare R2 file picker upload, Google OAuth consent UI, and Supabase integration

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2335569782"
+    serviceWorkerVersion: "489117215"
   }
 });
